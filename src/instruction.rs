7d2156e7b87af8d23a9bd6377c331442ceeec250
Özge <92800183+ozgekaplan12@users.noreply.github.com>
2024-02-08 20:00:45 +0300

    Add files via upload

---
+++ src/instruction.rs
@@ -0,0 +1,39 @@
+use borsh::BorshDeserialize;
+use borsh_derive::{BorshDeserialize, BorshSerialize};
+use solana_program::program_error::ProgramError;
+
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct UpdateArgs {
+    pub value: u32,
+}
+
+pub enum CounterInstructions {
+    Increment(u32),
+    Decrement(u32),
+    Update(UpdateArgs),
+    Reset,
+}
+
+impl CounterInstructions {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+
+        Ok(match variant {
+            0 => {
+                let (value_bytes, rest) = rest.split_at(std::mem::size_of::<u32>());
+                let value = u32::from_le_bytes(value_bytes.try_into().unwrap());
+                Self::Increment(value)
+            }
+            1 => {
+                let (value_bytes, rest) = rest.split_at(std::mem::size_of::<u32>());
+                let value = u32::from_le_bytes(value_bytes.try_into().unwrap());
+                Self::Decrement(value)
+            }
+            2 => Self::Update(UpdateArgs::try_from_slice(rest).unwrap()),
+            3 => Self::Reset,
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
